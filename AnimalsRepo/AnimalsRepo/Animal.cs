61b69876d4ee5716c980d61b421c801645c6152b
Refrimatik <info@refrimatik.com>
2016-11-02 18:29:24 +0100

    Heart property added

--- AnimalsRepo/AnimalsRepo/Animal.cs
+++ AnimalsRepo/AnimalsRepo/Animal.cs
@@ -5,7 +5,15 @@ using System.Text;
 
 namespace AnimalsRepo
 {
+    /// <summary>
+    /// Represent abstract representation of any animal.
+    /// </summary>
     public abstract class Animal
     {
+
+        /// <summary>
+        /// Informs if blood pump is working.
+        /// </summary>
+        public bool IsHeartWorking { get; set; }
     }
 }
